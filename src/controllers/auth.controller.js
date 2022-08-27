c40ffef2cf7d908904ba37cb925ad388a9d65218
anah <40556052+zulmianah@users.noreply.github.com>
2022-08-27 18:42:15 +0300

    Initial commit

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,68 @@
+const User = require('../models/user.model');
+const { hash: hashPassword, compare: comparePassword } = require('../utils/password');
+const { generate: generateToken } = require('../utils/token');
+
+exports.signup = (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+    const hashedPassword = hashPassword(password.trim());
+
+    const user = new User(firstname.trim(), lastname.trim(), email.trim(), hashedPassword);
+
+    User.create(user, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                status: "error",
+                message: err.message
+            });
+        } else {
+            const token = generateToken(data.id);
+            res.status(201).send({
+                status: "success",
+                data: {
+                    token,
+                    data
+                }
+            });
+        }
+    });
+};
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    User.findByEmail(email.trim(), (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    status: 'error',
+                    message: `User with email ${email} was not found`
+                });
+                return;
+            }
+            res.status(500).send({
+                status: 'error',
+                message: err.message
+            });
+            return;
+        }
+        if (data) {
+            if (comparePassword(password.trim(), data.password)) {
+                const token = generateToken(data.id);
+                res.status(200).send({
+                    status: 'success',
+                    data: {
+                        token,
+                        firstname: data.firstname,
+                        lastname: data.lastname,
+                        email: data.email
+                    }
+                });
+                return;
+            }
+            res.status(401).send({
+                status: 'error',
+                message: 'Incorrect password'
+            });
+        }
+    });
+
+}
---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+const db = require('../config/db.config');
+const { createNewUser: createNewUserQuery, findUserByEmail: findUserByEmailQuery } = require('../database/queries');
+const { logger } = require('../utils/logger');
+
+class User {
+    constructor(firstname, lastname, email, password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    static create(newUser, cb) {
+        db.query(createNewUserQuery, 
+            [
+                newUser.firstname, 
+                newUser.lastname, 
+                newUser.email, 
+                newUser.password
+            ], (err, res) => {
+                if (err) {
+                    logger.error(err.message);
+                    cb(err, null);
+                    return;
+                }
+                cb(null, {
+                    id: res.insertId,
+                    firstname: newUser.firstname,
+                    lastname: newUser.lastname,
+                    email: newUser.email
+                });
+        });
+    }
+
+    static findByEmail(email, cb) {
+        db.query(findUserByEmailQuery, email, (err, res) => {
+            if (err) {
+                logger.error(err.message);
+                cb(err, null);
+                return;
+            }
+            if (res.length) {
+                cb(null, res[0]);
+                return;
+            }
+            cb({ kind: "not_found" }, null);
+        })
+    }
+}
+
+module.exports = User;
