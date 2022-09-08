11cd42105b9d58657dd2ff61849ad87d9dcbe14d
anah <zulmianah@gmail.com>
2022-09-08 10:02:34 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  var message = "En cours de développement, revenez demain!\n",
-    version = "NodeJS " + process.versions.node + "\n",
-    response = [message].join("\n");
-  res.end(response);
-});
-server.listen();
