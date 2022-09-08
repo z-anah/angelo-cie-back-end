16b0037e342038a9ed263618a45738099ea72094
anah <zulmianah@gmail.com>
2022-09-08 19:48:20 +0300

    Revert "Update app.js"
    This reverts commit 990b68ed26e79c7296e3a7f53889b4dceea1aa19.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  var message = "En cours de développement, revenez demain!\n",
+  var message = "NOOB! It works!\n",
     version = "NodeJS " + process.versions.node + "\n",
-    response = [message].join("\n");
+    response = [message, version].join("\n");
   res.end(response);
 });
 server.listen();
