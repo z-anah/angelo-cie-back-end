708f876ba39eaa4c7e2d8d9f98dc2d7ca3e27afa
anah <zulmianah@gmail.com>
2022-09-08 19:49:06 +0300

    Revert "test"
    This reverts commit a06d6c0990f0ede9f45e551188c679c4687e0d23.

---
+++ src/views/_nuxt/18137ed.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{425:function(n,t,e){"use strict";e.r(t);var o={name:"AboutPage"},l=e(45),component=Object(l.a)(o,(function(){var n=this,t=n._self._c;return t("main",[t("h1",[n._v("ABOUT")]),n._v(" "),t("NuxtLink",{attrs:{to:"/"}},[n._v(" Index ")])],1)}),[],!1,null,null,null);t.default=component.exports}}]);
