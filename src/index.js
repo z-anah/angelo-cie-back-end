a06d6c0990f0ede9f45e551188c679c4687e0d23
anah <zulmianah@gmail.com>
2022-09-08 09:41:49 +0300

    test

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const { logger } = require('./utils/logger');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    logger.info(`Running on PORT ${PORT}`);
-});
