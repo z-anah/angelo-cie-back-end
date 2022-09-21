6a375c9a25bd3578ef364aada67bcdb6bf43100b
anah <zulmianah@gmail.com>
2022-09-21 09:30:33 +0300

    ok

--- src/views/_nuxt/2b25e96.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{255:function(e,n,l){"use strict";l.r(n);var t=l(1).a.extend({name:"IndexPage"}),o=l(57),component=Object(o.a)(t,(function(){var e=this,n=e._self._c;e._self._setupProxy;return n("p",[e._v("Welcome in admin Angelo & Cie panel")])}),[],!1,null,null,null);n.default=component.exports}}]);
