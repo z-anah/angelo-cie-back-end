75a1bd9470e5627a0777fd026f322e3cbacb7720
anah <zulmianah@gmail.com>
2022-10-07 19:06:45 +0300

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,19 +1,21 @@
 const mysql = require('mysql');
 const { logger } = require('../utils/logger');
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = require('../utils/secrets');
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = require('../utils/secrets');
 
 
 logger.info("anah", {
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASS,
-    database: DB_NAME
+    database: DB_NAME,
+    port: DB_PORT
 })
 const connection = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASS,
-    database: DB_NAME
+    database: DB_NAME,
+    port: DB_PORT
 });
 
 connection.connect((err) => {
